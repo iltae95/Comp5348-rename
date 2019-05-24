f21693ecf369d766949614e8c415ea23b9c0ee5d
Unknown <park.alex01@gmail.com>
2019-05-24 18:11:05 +1000

    Add MessageBus Project

---
+++ GroupProject/Common/Interfaces/ISubscriberService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using Common.Model;
+
+namespace Common.Interfaces
+{
+    [ServiceContract]
+    public interface ISubscriberService
+    {
+        [OperationContract(IsOneWay = true)]
+        void PublishToSubscriber(Message pMessage);
+    }
+}
---
+++ GroupProject/MessageBus/Services/PublisherService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Common.Model;
+using Common.Interfaces;
+
+namespace MessageBus
+{
+    public class PublisherService : IPublisherService
+    {
+        public void Publish(Message pMessage)
+        {
+            foreach (String lHandlerAddress in SubscriptionRegistry.Instance.GetTopicSubscribers(pMessage.Topic))
+            {
+                ISubscriberService lSubServ = ServiceFactory.GetService<ISubscriberService>(lHandlerAddress);
+                lSubServ.PublishToSubscriber(pMessage);
+            }
+        }
+    }
+}
